f28f197d98534fb91509c53df1fa58e3640cc343
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-06-16 17:34:51 -0700

    wire up prisma to register page--still need to work on form validation

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function POST(req: Request) {
+  const data = await req.json();
+  const { firstName, lastName, birthday, email } = data;
+
+  try {
+    await prisma.user.create({
+      data: {
+        firstName: firstName,
+        lastName: lastName,
+        birthday: birthday,
+        email: email,
+      },
+    });
+
+    return NextResponse.json(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
