55b74b9043687604aa441657ddb7e2303a64269d
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-06-19 15:49:05 -0700

    basic next-auth sign-in functionality in place

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,41 @@
 import EmailProvider from "next-auth/providers/email";
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import { Adapter } from "next-auth/adapters";
+import nodemailer from "nodemailer";
+
+const THIRTY_DAYS = 60 * 60 * 24 * 30;
+const THIRTY_MINUTES = 60 * 30;
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma) as Adapter,
+  debug: true,
+  pages: {
+    signIn: "/login",
+    verifyRequest: "/verify-request",
+  },
   providers: [
     EmailProvider({
-      server: process.env.EMAIL_SERVER,
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
       from: process.env.EMAIL_FROM,
     }),
   ],
+  secret: process.env.SECRET,
+  session: {
+    strategy: "jwt",
+    maxAge: THIRTY_DAYS,
+    updateAge: THIRTY_MINUTES,
+  },
 });
 
 export { handler as GET, handler as POST };
