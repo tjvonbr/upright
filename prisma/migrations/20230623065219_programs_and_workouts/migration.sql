452dff7a52b179baa76b0cb0316d9186cc507874
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-06-22 23:54:28 -0700

    programs and workouts migration

---
+++ prisma/migrations/20230623065219_programs_and_workouts/migration.sql
@@ -0,0 +1,53 @@
+-- CreateTable
+CREATE TABLE "programs" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "programs_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "workouts" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "date" DATE NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "programId" INTEGER,
+
+    CONSTRAINT "workouts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_ExerciseToWorkout" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "programs_name_key" ON "programs"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "workouts_name_key" ON "workouts"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ExerciseToWorkout_AB_unique" ON "_ExerciseToWorkout"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ExerciseToWorkout_B_index" ON "_ExerciseToWorkout"("B");
+
+-- AddForeignKey
+ALTER TABLE "programs" ADD CONSTRAINT "programs_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "workouts" ADD CONSTRAINT "workouts_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "workouts" ADD CONSTRAINT "workouts_programId_fkey" FOREIGN KEY ("programId") REFERENCES "programs"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ExerciseToWorkout" ADD CONSTRAINT "_ExerciseToWorkout_A_fkey" FOREIGN KEY ("A") REFERENCES "exercises"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ExerciseToWorkout" ADD CONSTRAINT "_ExerciseToWorkout_B_fkey" FOREIGN KEY ("B") REFERENCES "workouts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
