1ac354ee04b418247ebe1e82201d579c4ae45d4e
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-06-25 23:47:42 -0700

    removed more files with unused vars

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,7 @@
 import Header from "./components/Header";
 import MainNavbar from "../components/MainNavbar";
-import { navbarItems, subNavbarItems } from "../config/navigation";
+import { navbarItems } from "../config/navigation";
 import { redirect } from "next/navigation";
-import SubNavbar from "../components/SubNavbar";
 import { User } from "next-auth";
 import { getCurrentUser } from "../lib/session";
 import React from "react";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
 import "semantic-ui-css/semantic.min.css";
 
-const Loading = () => {
-  return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center bg-white">
-      <span className="text-2xl">&#128200;</span>
-      <h1 className="font-medium">Loading your data...</h1>
-    </div>
-  );
-};
-
 export default function Home() {
   return <div>Hello</div>;
 }
