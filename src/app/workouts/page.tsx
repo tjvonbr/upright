3047fdfb8bb39bbf77b6f50e659775e5a86af710
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-07-21 01:58:27 -0700

    add exercise to workout

--- src/app/workouts/page.tsx
+++ src/app/workouts/page.tsx
@@ -1,6 +1,9 @@
 import { Program, Workout } from "@prisma/client";
 import Link from "next/link";
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+import { getCurrentUser } from "@/lib/session";
 
 import { getWorkoutsForProgram } from "../programs/[programId]/page";
 
@@ -8,17 +11,31 @@ interface ProgramWorkoutsProps {
   params: { programId: Program["id"] };
 }
 
-export default async function ProgramWorkouts({
-  params,
-}: ProgramWorkoutsProps) {
-  const workouts = await getWorkoutsForProgram(params.programId);
+export async function getWorkoutsForUser(userId: string) {
+  const workouts = db.workout.findMany({
+    where: {
+      userId: Number(userId),
+    },
+  });
+
+  return workouts;
+}
+
+export default async function ProgramWorkouts() {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect("/login");
+  }
+
+  const workouts = await getWorkoutsForUser(user?.id);
 
   if (!workouts) {
     return notFound();
   }
 
   return (
-    <div>
+    <div className="flex flex-col">
       <h1>Workouts</h1>
       {workouts.map((workout: Workout, idx: number) => (
         <Link key={idx} href={`/workouts/${workout.id}`}>
