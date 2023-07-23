d9fc45c4642894f21cd0cb815d542e5dc2ae16da
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-07-23 01:07:36 -0700

    relocated add workout page

--- src/app/workouts/page.tsx
+++ src/app/workouts/page.tsx
@@ -1,32 +1,26 @@
-import { Workout } from "@prisma/client";
-import Link from "next/link";
 import { notFound, redirect } from "next/navigation";
 
+import { getProgramsForUser } from "@/lib/api/programs";
 import { getWorkoutsForUser } from "@/lib/api/workouts";
 import { getCurrentUser } from "@/lib/session";
 
-export default async function ProgramWorkouts() {
+import WorkoutsOperations from "../components/WorkoutsOperations";
+
+export default async function Workouts() {
   const user = await getCurrentUser();
 
   if (!user) {
     redirect("/login");
   }
 
-  // See if this works
+  const programs = await getProgramsForUser(user?.id);
   const workouts = await getWorkoutsForUser(user?.id);
 
-  if (!workouts) {
+  if (!workouts || !programs) {
     return notFound();
   }
 
   return (
-    <div className="flex flex-col">
-      <h1>Workouts</h1>
-      {workouts.map((workout: Workout, idx: number) => (
-        <Link key={idx} href={`/workouts/${workout.id}`}>
-          {workout.name}
-        </Link>
-      ))}
-    </div>
+    <WorkoutsOperations user={user} programs={programs} workouts={workouts} />
   );
 }
