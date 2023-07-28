0b4e4c52e3ebc6042b667266f68319e784cc5713
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-07-28 03:27:25 -0700

    removed unneccesary font files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,6 @@
-import "@/styles/globals.css";
-
-import { Inter as FontSans } from "next/font/google";
-import localFont from "next/font/local";
-import { twJoin } from "tailwind-merge";
+import "../app/global.css";
 
 import { Toaster } from "@/components/common/toaster";
-
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
-
-// Font files can be colocated inside of `pages`
-const fontHeading = localFont({
-  src: "../assets/fonts/CalSans-SemiBold.woff2",
-  variable: "--font-heading",
-});
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -25,13 +10,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body
-        className={twJoin(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
-          fontHeading.variable
-        )}
-      >
+      <body className="min-h-screen bg-background font-sans antialiased">
         {children}
         <Toaster />
       </body>
