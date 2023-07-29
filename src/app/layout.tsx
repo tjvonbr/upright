425b48563da572c7c9aa7553df26e922cda465a8
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-07-29 02:49:22 -0700

    return next-auth SessionProvider at root level

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,24 @@
 import "./globals.css";
 
-import { Toaster } from "@/components/common/toaster";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
 
+import { Toaster } from "@/components/common/toaster";
 interface RootLayoutProps {
   children: React.ReactNode;
+  session: Session;
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children, session }: RootLayoutProps) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head />
-      <body className="min-h-screen bg-background font-sans antialiased">
-        {children}
-        <Toaster />
-      </body>
-    </html>
+    <SessionProvider session={session}>
+      <html lang="en" suppressHydrationWarning>
+        <head />
+        <body className="min-h-screen bg-background font-sans antialiased">
+          {children}
+          <Toaster />
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
