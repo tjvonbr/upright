5bddcd4b74aab6e6c828b764581ee4b5df7a9c93
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-08-02 00:18:56 -0700

    updated verify-request UI

--- src/app/verify-request/page.tsx
+++ src/app/verify-request/page.tsx
@@ -1,20 +1,34 @@
+import { MoveUpRight } from "lucide-react";
+import { Metadata } from "next";
 import Link from "next/link";
+import { twJoin } from "tailwind-merge";
 
-const VerifyRequest = () => {
-  return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center bg-white">
-      <div className="h-[100px] w-[500px] flex flex-col justify-center items-center">
-        <p className="text-2xl">&#128200;</p>
-        <h1 className="font-semibold text-center">
-          A login link has been sent to your email. If you don&#39;t receive it,
-          please confirm you entered the correct email address.
-        </h1>
-      </div>
-      <Link className="text-indigo-500 font-medium" href="/login">
-        Go back to login
-      </Link>
-    </div>
-  );
+import { buttonVariants } from "../components/common/button";
+
+export const metadata: Metadata = {
+  title: "Verify Login Request",
+  description: "Verify your login request",
 };
 
-export default VerifyRequest;
+export default function VerifyRequest() {
+  return (
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
+      <div className="w-1/3 flex flex-col justify-center items-center space-y-4">
+        <Link href="/">
+          <MoveUpRight color="black" size={25} strokeWidth={2.5} />
+        </Link>
+        <h1 className="text-2xl font-bold">Please check your inbox</h1>
+        <p className="w-3/4 text-center text-slate-500">
+          We just sent you a login link to the email you provided. If you
+          don&apos; receive one, make you sure you entered the correct email.
+        </p>
+        <Link
+          className={twJoin(buttonVariants({ variant: "primary" }))}
+          href="/login"
+        >
+          Head back to the login page
+        </Link>
+      </div>
+    </div>
+  );
+}
