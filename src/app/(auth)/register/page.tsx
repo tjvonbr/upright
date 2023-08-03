ad5bf1c28f6d7f4d5a9128d90dd0bf3dbb8c9dc5
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-08-02 18:59:42 -0700

    added register form client component

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,113 +1,25 @@
-"use client";
-
+import { MoveUpRight } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
 
-import Spinner from "../../components/Spinner";
+import RegisterForm from "@/components/RegisterForm";
 
-interface FormElements extends HTMLFormControlsCollection {
-  usernameInput: HTMLInputElement;
-}
-interface UserFormElement extends HTMLFormElement {
-  readonly elements: FormElements;
-}
-
-const Register = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [birthday, setBirthday] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
-
-  async function handleSubmit(event: React.FormEvent<UserFormElement>) {
-    setIsLoading(true);
-    event.preventDefault();
-
-    try {
-      await fetch("../api/register", {
-        method: "POST",
-        body: JSON.stringify({
-          firstName,
-          lastName,
-          email,
-          birthday: new Date(birthday),
-        }),
-      });
-      setIsLoading(false);
-    } catch (err) {
-      throw new Error(`Error: ${err}`);
-    }
-  }
-
+export default function Register() {
   return (
-    <div className="h-screen w-screen flex flex-col items-center justify-center bg-white">
-      <form action="" onSubmit={handleSubmit}>
-        <div className="h-[550px] w-[500px] flex flex-col justify-around items-center rounded-md shadow-gray-200 shadow-lg">
-          <div className="w-4/5">
-            <h3 className="text-2xl font-semibold self-start">
-              Create your Up+Right account
-            </h3>
-          </div>
-
-          <div className="flex flex-col justify-around w-4/5">
-            <label className="text-sm font-medium" htmlFor="">
-              First name
-            </label>
-            <input
-              className="my-2 border border-slate-300 rounded-md py-2 px-2"
-              type="text"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setFirstName(e.target.value)
-              }
-            />
-            <label className="text-sm font-medium" htmlFor="">
-              Last name
-            </label>
-            <input
-              className="my-2 border border-slate-300 rounded-md py-2 px-2"
-              type="text"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setLastName(e.target.value)
-              }
-            />
-            <label className="text-sm font-medium" htmlFor="">
-              Email
-            </label>
-            <input
-              className="my-2 border border-slate-300 rounded-md py-2 px-2"
-              type="text"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setEmail(e.target.value)
-              }
-            />
-            <label className="text-sm font-medium" htmlFor="">
-              Birthday
-            </label>
-            <input
-              className="my-2 border border-slate-300 rounded-md py-2 px-2"
-              placeholder="mm/dd/yyyy"
-              type="text"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setBirthday(e.target.value)
-              }
-            />
-          </div>
-          <button
-            className="h-[50px] w-4/5 bg-indigo-500 text-white font-medium rounded-md flex justify-center items-center"
-            type="submit"
-          >
-            {isLoading ? <Spinner /> : "Continue"}
-          </button>
-        </div>
-      </form>
-      <div className="m-5">
-        Already have an account?{" "}
-        <Link className="text-indigo-500" href="/login">
-          Login
+    <div className="h-screen w-screen flex flex-col items-center justify-center">
+      <div className="w-1/3 flex flex-col justify-center items-center space-y-2">
+        <Link href="/">
+          <MoveUpRight color="black" size={25} strokeWidth={2.5} />
+        </Link>
+        <h1 className="text-2xl font-bold">Sign up for Upright</h1>
+        <p className="w-3/4 text-center text-slate-500">
+          Please enter your personal information below so we can get your
+          started
+        </p>
+        <RegisterForm />
+        <Link className="text-sm text-slate-500 hover:underline" href="/login">
+          Already have an account? Login here
         </Link>
       </div>
     </div>
   );
-};
-
-export default Register;
+}
---
+++ src/components/RegisterForm.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { twJoin } from "tailwind-merge";
+
+import { Button, buttonVariants } from "@/app/components/common/button";
+import Input from "@/app/components/common/Input";
+
+import Spinner from "./Spinner";
+
+export default function RegisterForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [isMutating, setIsMutating] = useState(false);
+
+  const router = useRouter();
+
+  async function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+    setIsMutating(true);
+
+    const response = await fetch("../api/register", {
+      method: "POST",
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        birthday: new Date(birthday),
+      }),
+    });
+
+    if (response.ok) {
+      setIsMutating(false);
+      router.push("/login");
+    }
+  }
+
+  return (
+    <form className="w-3/4 flex flex-col space-y-2" onSubmit={handleSubmit}>
+      <fieldset>
+        <label className="m-0 p-0 text-sm font-medium" htmlFor="">
+          First name
+        </label>
+        <Input
+          className="h-10 w-full px-3 py-2 border border-slate-200 rounded-md"
+          name="firstName"
+          type="text"
+          value={firstName}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setFirstName(e.target.value)
+          }
+        />
+      </fieldset>
+      <fieldset>
+        <label className="m-0 p-0 text-sm font-medium" htmlFor="">
+          Last name
+        </label>
+        <Input
+          className="h-10 w-full px-3 py-2 border border-slate-200 rounded-md"
+          name="lastName"
+          type="text"
+          value={lastName}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setLastName(e.target.value)
+          }
+        />
+      </fieldset>
+      <fieldset>
+        <label className="m-0 p-0 text-sm font-medium" htmlFor="">
+          Email
+        </label>
+        <Input
+          className="h-10 w-full px-3 py-2 border border-slate-200 rounded-md"
+          name="email"
+          type="email"
+          value={email}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setEmail(e.target.value)
+          }
+        />
+      </fieldset>
+      <fieldset>
+        <label className="m-0 p-0 text-sm font-medium" htmlFor="">
+          Birthday
+        </label>
+        <Input
+          className="h-10 w-full px-3 py-2 border border-slate-200 rounded-md"
+          placeholder="mm/dd/yyyy"
+          name="birthday"
+          type="text"
+          value={birthday}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setBirthday(e.target.value)
+          }
+        />
+      </fieldset>
+      <Button
+        className={twJoin(buttonVariants({ variant: "primary" }))}
+        type="submit"
+      >
+        {isMutating ? <Spinner /> : "Continue"}
+      </Button>
+    </form>
+  );
+}
