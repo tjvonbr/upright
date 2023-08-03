43b1d159fc6f6791fd770a1f75297135791f8869
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2023-08-03 12:07:44 -0700

    navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,8 +2,9 @@ import { redirect } from "next/navigation";
 import React from "react";
 
 import UserNav from "@/components/UserNav";
+import { navbarItems } from "@/config/navigation";
 
-import { getCurrentUser } from "../../../lib/session";
+import { getCurrentUser } from "../../lib/session";
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
@@ -21,7 +22,7 @@ export default async function DashboardLayout({
   return (
     <div className="min-h-screen flex flex-col">
       <header className="h-10 py-2 px-4 flex justify-between items-center border-b border-slate-200">
-        <UserNav />
+        <UserNav navbarItems={navbarItems.dashboard} />
       </header>
       <main>{children}</main>
     </div>
--- src/components/UserNav.tsx
+++ src/components/UserNav.tsx
@@ -6,16 +6,33 @@ import { signOut } from "next-auth/react";
 import { twJoin } from "tailwind-merge";
 
 import { buttonVariants } from "@/app/components/common/button";
+import { NavbarItem } from "@/config/navigation";
 
-export default function UserNav() {
+interface UserNavProps {
+  navbarItems?: NavbarItem[];
+}
+
+export default function UserNav({ navbarItems }: UserNavProps) {
   return (
     <div className="w-full flex items-center justify-between">
-      <Link className="flex items-center space-x-1" href="/">
-        <MoveUpRight color="black" size={18} strokeWidth={2.5} />
-        <span className="font-bold text-xl text-black hover:text-black">
-          Upright
-        </span>
-      </Link>
+      <div className="flex items-center space-x-6">
+        <Link className="flex items-center space-x-1" href="/">
+          <MoveUpRight color="black" size={18} strokeWidth={2.5} />
+          <span className="font-bold text-xl text-black hover:text-black">
+            Upright
+          </span>
+        </Link>
+        {navbarItems &&
+          navbarItems.map((item, idx: number) => (
+            <Link
+              className="text-sm text-slate-500 hover:text-black transition-colors"
+              key={idx}
+              href={item.href}
+            >
+              {item.name}
+            </Link>
+          ))}
+      </div>
       <button
         className={twJoin(buttonVariants({ variant: "ghost" }))}
         onClick={() => signOut({ callbackUrl: "/login" })}
